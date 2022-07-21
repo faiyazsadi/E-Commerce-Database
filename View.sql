5c10380542d2b8c4d8885c6f1965339c99fad46c
faiyazsadi <sadi1807073@stud.kuet.ac.bd>
2022-07-21 06:55:30 +0600

    view deeznutz

---
+++ View.sql
@@ -0,0 +1,28 @@
+create view user_info as
+select fname, lname
+from customer
+where customer_id >= 71 and customer_id <= 80;
+
+select * from user_info;
+
+drop view user_info;
+
+create view info as
+select customer.fname, customer.lname, payment.payment_method
+from customer
+inner join payment
+on customer.order_id = payment.order_id
+where payment.payment_method = 'Bkash';
+
+select * from info;
+
+create or replace view info as
+select distinct customer.fname, customer.lname, payment.payment_method
+from customer
+inner join payment
+on customer.order_id = payment.order_id
+where payment.payment_method = 'Nagad';
+
+select * from info;
+
+drop view info;
